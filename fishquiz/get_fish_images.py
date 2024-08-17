2617030d6dfe71fd847ce6195acc5fce893c8056
english8192 <evantenglish@gmail.com>
2024-08-17 09:55:34 -0500

    fish

--- fishquiz/get_fish_images.py
+++ fishquiz/get_fish_images.py
@@ -26,7 +26,7 @@ def get_images(query):
     if query == "test":
         driver.get(f"https://www.google.com/search?q={query}&sca_esv=cd1c1d76151aa7bf&sca_upv=1&biw=1912&bih=922&udm=2&sxsrf=ADLYWIKWrlpgLytBqXYUPHUvSx2iSRJG1Q%3A1723560173443&ei=7XC7Zv7VGr-hptQPsYCemQc&ved=0ahUKEwi-gIz_mfKHAxW_kIkEHTGAJ3MQ4dUDCBA&uact=5&oq=epic+systems&gs_lp=Egxnd3Mtd2l6LXNlcnAiDGVwaWMgc3lzdGVtczILEAAYgAQYsQMYgwEyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARI9QxQAFibDHAAeACQAQGYAawBoAHDBqoBBDEwLjK4AQPIAQD4AQGYAgugArAFwgIEECMYJ8ICChAAGIAEGEMYigXCAggQABiABBixA8ICDhAAGIAEGLEDGIMBGIoFwgIEEAAYA5gDAJIHBDEwLjGgB5ZA&sclient=gws-wiz-serp")
 
-        time.sleep(2)
+        time.sleep(5)
     else:
         actions = ActionChains(driver)
         # Send '/' and then Ctrl+A
@@ -35,7 +35,7 @@ def get_images(query):
         s = f"{query} fish"
         actions.send_keys(s + Keys.ENTER)
         actions.perform()
-        time.sleep(2)
+        time.sleep(5)
         # Retrieve the first five image elements
         image_elements = [i for i in driver.find_elements(By.CSS_SELECTOR, 'img') if int(i.get_attribute("width"))>46 and i.get_attribute("alt")!="Google"][0:5]
 
@@ -46,7 +46,7 @@ def get_images(query):
         for i, image_element in enumerate(image_elements):
         #     # Click on the image using JavaScript
             driver.execute_script("arguments[0].click();", image_element)
-            time.sleep(2)  # Wait for the image to load
+            time.sleep(5)  # Wait for the image to load
 
             # Optionally, extract the URL of the larger image
             substrings = ["data:","encrypted","gstatic","googlelogo"]
@@ -60,17 +60,20 @@ def get_images(query):
                 continue
 
 
-            response = requests.get(large_image_url)
-            if response.status_code == 200:
-
-                image_path = os.path.join(query_dir, f'{query}_{i}.jpg')
-                with open(image_path, 'wb') as file:
-                    file.write(response.content)
-                print(f"Downloaded {image_path}")
-
+            try:
+                response = requests.get(large_image_url, timeout=10)  # 10-second timeout
+                if response.status_code == 200:
+                    image_path = os.path.join(query_dir, f'{query}_{i}.jpg')
+                    with open(image_path, 'wb') as file:
+                        file.write(response.content)
+                    print(f"Downloaded {image_path}")
+                else:
+                    print(f"Failed to download image for query: {query} - Status code: {response.status_code}")
+            except requests.exceptions.RequestException as e:
+                print(f"Request failed for URL: {large_image_url} - {e}")
             # Go back to the search results
             driver.back()
-            time.sleep(1)
+            time.sleep(5)
 
 
 
