1909546c8f267d3e92a515816b19eb67e274fe09
Evan English <eenglish@epic.com>
2024-08-17 11:14:12 -0500

    geotrivia update

--- geotrivia/script.js
+++ geotrivia/script.js
@@ -4,6 +4,8 @@ let currentQuestion = [];
 let correctCount= 0
 let incorrectCount=0
 let gameState= 1
+let numGroups = 74
+let filePaths = Array()
 
 async function loadData(filePaths) {
     try {
@@ -42,7 +44,7 @@ function displayQuestion() {
     if (!currentQuestion) return; // Stop if there are no more questions
     document.getElementById("question").textContent = currentQuestion[0];
     document.getElementById("answer").textContent = ""
-
+    console.log("displayQuestion")
     return currentQuestion
 }
 
@@ -66,14 +68,21 @@ function incorrectClicked() {
   }
 }
 
+function resetCounts() {
+    correctCount = 0 ;
+    document.getElementById("correctCount").textContent = correctCount
+    incorrectCount = 0 ;
+    document.getElementById("incorrectCount").textContent = incorrectCount
+}
 
- function endGame() {
+
+function endGame() {
     document.getElementById("question").textContent  ="GAME OVER"
     gameState = 0
  }
 
 
- function handleKeyPress(event) {
+function handleKeyPress(event) {
     if (event.key === "s") {
       event.preventDefault(); // Prevent default form submission behavior
       showAnswer();
@@ -83,22 +92,75 @@ function incorrectClicked() {
     } else if (event.key === "p") {
         event.preventDefault();
         incorrectClicked();
-      }
+        }
   }
 
+function getGroupNames() {
+    let groupList = Array()
+    for (let i = 1; i <= numGroups; i++ ) {
+        groupList.push(`groups/group_${i}.json`)
+        console.log(groupList[i])
+    }
+    return groupList
+}
 
 
 
+function chooseQuestionLists() {
+    filePaths = Array()
+    const activeGroups = document.getElementsByClassName('groupButton active')
+    //const inactiveGroups = document.getElementsByClassName('groupButton')
+    for (let i = 0; i < activeGroups.length; i++) {
+        const formattedString = `groups/group_${activeGroups[i].textContent}.json`; // convert the textcontent that's just a number back to the appropriate fileneame
+        filePaths.push(formattedString) // this needs to be the filename
 
+    }
+    console.log(filePaths)
+    //init()
+
+}
+
+
+
+const groupLabels = getGroupNames() //array of filenames groups/group_44.json
+
+const buttonsContainer = document.getElementById('groupButtons') // group button container elemetn
+
+function toggleButton(event) {
+    event.target.classList.toggle('active')  //add class 'active' when toggleButton called
+}
+
+groupLabels.forEach(label => {
+    let tempLabel = label.replace(/[^0-9]/g, ''); //convert filename to single number
+    const button = document.createElement('button')
+    button.textContent = tempLabel
+
+    button.className = 'groupButton'
+    button.addEventListener('click',toggleButton)
+    buttonsContainer.appendChild(button)
+})
 
 
 
   async function init() {
-    const filePaths = ['groups/group_1.json', 'groups/group_2.json']; // Add more file paths as needed
+    gameState = 1
+    resetCounts()
+
+
+
     await loadData(filePaths); // Load the data first
     displayQuestion(); // Then display the first question
     document.addEventListener('keydown', handleKeyPress);
+
 }
+
+buttonsContainer.addEventListener('click',function(event) {
+    if (event.target.classList.contains('groupButton')) {
+        console.log("group button clicked")
+        chooseQuestionLists()
+        init()
+    }
+})
 
 init(); // Start the process
 
